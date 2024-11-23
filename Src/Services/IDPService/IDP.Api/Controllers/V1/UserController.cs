55bddef46b1fe698e27b86975b859a903de66353
Sobhan Ezzati <SobhanEzzati95@gmail.com>
2024-11-23 08:45:38 +0330

    Add project files.

---
+++ Src/Services/IDPService/IDP.Api/Controllers/V1/UserController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.V1
+{
+    [Route("api/v1/user")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+    }
+}
